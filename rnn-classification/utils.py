f1fca614750cd419313400797caaab028d308891
Chirayu <chirayu47sharma@gmail.com>
2025-08-09 02:27:05 +0530

    imports

--- rnn-classification/utils.py
+++ rnn-classification/utils.py
@@ -1,4 +1,11 @@
 import torch
-import torch.nn as nn
-import torchvision
+import random
 
+import io
+import os
+import unicodedata
+import string 
+import glob
+
+# alphabets small + capital letters + " .,;"
+ALL_Letters = string.ascii_letters  + " .,;"
