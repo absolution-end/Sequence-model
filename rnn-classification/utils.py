7072061b7f4df88758c09e9a86fadad50ab8bff5
Chirayu <chirayu47sharma@gmail.com>
2025-08-09 23:03:04 +0530

    line split and ascii conversion

--- rnn-classification/utils.py
+++ rnn-classification/utils.py
@@ -9,3 +9,32 @@ import glob
 
 # alphabets small + capital letters + " .,;"
 ALL_Letters = string.ascii_letters  + " .,;"
+
+# turn a unicode string to a plan ASCII
+def unicode_to_ascii(s):
+    return " ".join(
+        c for c in unicodedata.normalize('NFD',s)
+        if unicodedata.category(c) != 'Mn'
+        and c in ALL_Letters
+    )
+    
+def load_data():
+    ''' Build category_lines directionary, a list of name per language'''
+    category_lines = {}
+    all_categorys = []
+    
+    def find_files(path):
+        return glob.glob(path)
+    
+    def read_lines(filename):
+        lines = io.open(filename, encoding ='utf-8').read().strip().split('\n')
+        return [unicode_to_ascii(lines) for line in lines ]
+    
+    for filename in find_files('data/names/*.txt'):
+        category = os.path.splitext(os.path.basename(filename))[0]
+        all_categorys.append(category)
+        
+        lines = read_lines(filename)
+        category_lines[category] = lines
+        
+    return category_lines, all_categorys
